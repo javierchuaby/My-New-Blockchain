959bb6e7f3ca120d42f6e755fbd5e0af93151e54
Javier Chua <161703315+javierchuaby@users.noreply.github.com>
2025-09-15 17:15:40 +0800

    Updated calculateHash

--- Block.java
+++ Block.java
@@ -1,36 +1,38 @@
-import java.util.Date;
-
 import utils.StringUtil;
+
+import java.util.Date;
 
 public class Block {
 
-	public String hash;
-	public String previousHash;
-	private String data; //our data will be a simple message.
-	private long timeStamp; //as number of milliseconds since 1/1/1970.
+    public String hash;
+    public String previousHash;
+    private String data; //our data will be a simple message.
+    private long timeStamp; //as number of milliseconds since 1/1/1970.
     private int nonce;
 
-	//Block Constructor
-	public Block(String data,String previousHash ) {
-		this.data = data;
-		this.previousHash = previousHash;
-		this.timeStamp = new Date().getTime();
+    //Block Constructor
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
-	}
+    }
 
+    //Calculate new hash based on blocks contents
     public String calculateHash() {
-        String calculatedhash = StringUtil.applySha256( 
+        String calculatedhash = StringUtil.applySha256(
                 previousHash +
-                Long.toString(timeStamp) +
-                data 
-                );
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+        );
         return calculatedhash;
     }
 
     public void mineBlock(int difficulty) {
         String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
-        while(!hash.substring( 0, difficulty).equals(target)) {
-            nonce ++;
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!!! : " + hash);
